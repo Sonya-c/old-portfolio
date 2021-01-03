324f36f9eea47acdd24bf5dea10bd7a8b8ca32a3
Sonya <sonya.castro.gomez@gmail.com>
2021-01-03 17:35:45 -0500

    detalles y contenidos

--- JS/modalImg.js
+++ JS/modalImg.js
@@ -1,11 +1,14 @@
 const MODAL = document.getElementById("modal-img");
 const MODAl_IMG = document.getElementById("modal-img-image");
+const BODY = document.body;
 
 function closeModal() {
     MODAL.style.display = "none";
+    BODY.style.overflow = "auto";
 }
 
 function openModal(img) {
     MODAl_IMG.src = img.src;
     MODAL.style.display = "block";
+    BODY.style.overflow = "hidden";
 }
